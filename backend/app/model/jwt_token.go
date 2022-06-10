7515373d393d3d3f90a1c1ad0c53cd3a769a7257
uloydev <uloydev@gmail.com>
2022-06-10 15:42:00 +0700

    [DELETE] unused files

--- backend/app/model/jwt_token.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type JwtToken struct {
-	AccessToken        string `json:"access_token"`
-	AccessTokenExpire  int    `json:"access_token_expire"`
-	RefreshToken       string `json:"refresh_token"`
-	RefreshTokenExpire int    `json:"refresh_token_expire"`
-}
--- backend/app/model/reset_password.go
+++
@@ -1,5 +0,0 @@
-package model
-
-type ResetPasswordRequest struct {
-	Email string `json:"email" example:"user@toqcer.id"`
-}
