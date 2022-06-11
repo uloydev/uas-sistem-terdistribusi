389d86f55b5d20d5a794cc26feb855024ef0cd5e
fathrahh <gazoon3@gmail.com>
2022-06-11 15:45:09 +0700

    [ADD] SidebarLink

--- frontend/src/components/SidebarLink.tsx
+++ frontend/src/components/SidebarLink.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import { IconType } from 'react-icons'
 
 interface SidebarLinkProps {
     href: string
     children: React.ReactNode
     className?: string
-    Icon: 
+    Icon?: IconType
 }
 
 const SidebarLink: React.FC<SidebarLinkProps> = ({ Icon, href, children, className }) => {
     return (
-        <li className=''>
-            <Icon />
+        <li className='cursor-pointer p-5 hover:brightness-110 bg-primary flex items-center gap-x-4 text-slate-300'>
+            {Icon && <Icon />}
             <Link href={href}>
                 <span className={`${className} `}>{children}</span>
             </Link>
