2b2a1b0b254e18f4fa39ab4c62b740215bd428dc
fathrahh <gazoon3@gmail.com>
2022-06-11 15:44:51 +0700

    [ADD] Sidebar components

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react'
 import SidebarLink from './SidebarLink'
-import styles from "/styles/Sidebar.module.css"
+import { BiTable, BiPencil } from 'react-icons/bi'
 
 const Sidebar: FC = () => {
 
     return (
-        <aside className={`${styles.sidebar} bg-primary`}>
-            <h4 className={`${styles.header} font-semibold`}>Warement</h4>
+        <aside className={`bg-primary  h-screen w-[285px] pb-10 rounded-r-lg`}>
+            <div className='px-3 mb-3'>
+                <h4 className={`after:content-[''] after:h-0.5 after:rounded-full after:w-full after:bg-slate-200 after:absolute after:bottom-0 after:left-0 relative text-center text-slate-200 text-xl font-bold py-5`}>
+                    Warement
+                </h4>
+            </div>
             <ul>
-                <SidebarLink href="/">Product Catalog</SidebarLink>
-                <SidebarLink href="/">Product Edit</SidebarLink>
+                <SidebarLink Icon={BiTable} href="/">Product Catalog</SidebarLink>
+                <SidebarLink Icon={BiPencil} href="/">Product Edit</SidebarLink>
             </ul>
         </aside>
     )
