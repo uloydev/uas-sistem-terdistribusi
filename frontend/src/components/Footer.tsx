198137f84962057abcf20ce20ca43bf954269d5c
fathrahh <gazoon3@gmail.com>
2022-06-11 17:49:54 +0700

    [ADD] component footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='py-6  text-center'>Created by ijordev feat uloydev</footer>
+    )
+}
+
+export default Footer
