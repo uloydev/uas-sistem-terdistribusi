7a0bba2c732348faf27b880c4726fb16331680f9
fathrahh <gazoon3@gmail.com>
2022-06-13 04:39:52 +0700

    aaa

--- frontend-client/utils/config.ts
+++ frontend-client/utils/config.ts
@@ -1,6 +1,6 @@
 export const config = {
     headers: {
-        "Referer": "https://www.warement.uloy.dev.com/",
+        "Referer": "https://www.warement.uloy.dev/",
         "Referrer-Policy": "strict-origin-when-cross-origin",
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
